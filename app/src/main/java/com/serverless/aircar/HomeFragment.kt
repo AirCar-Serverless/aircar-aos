eccffb668e4db9e131ad9f5dc7a240d0ce55f2eb
jaemin-Yoo <iamjm29@naver.com>
2022-08-24 23:38:24 +0900

    Feat: kakao map api 연동

--- app/src/main/java/com/serverless/aircar/HomeFragment.kt
+++ app/src/main/java/com/serverless/aircar/HomeFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.serverless.aircar.databinding.FragmentHomeBinding
+import net.daum.mf.map.api.MapView
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
         context ?: return binding.root
+
+        val mapView = MapView(context)
+        binding.mapView.addView(mapView)
 
         return binding.root
     }
